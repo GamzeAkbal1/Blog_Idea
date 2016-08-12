8e9042e320ff95545ab7b531b3e41623f3142846
GamzeAkbal1 <gakbal112@gmail.com>
2016-08-12 14:39:22 +0300

    Add edit function

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'ideas', to: 'ideas#index'
   get 'ideas/:id', to: 'ideas#show', as: "idea"
   post 'ideas', to: 'ideas#create'
-  get 'ideas/:id/edit', to:'ideas#edit'
+  get 'ideas/:id/edit', to:'ideas#edit', as: "edit_idea"
   put 'ideas/:id', to: 'ideas#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
