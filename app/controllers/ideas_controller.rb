1f5662cb00c9670d86905a332ec20c4ae2409a2f
GamzeAkbal1 <gakbal112@gmail.com>
2016-08-12 11:37:29 +0300

    Idea Blog

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,17 +1,42 @@
 class IdeasController < ApplicationController
 	def new
-		@name = "Saygin"
+		@idea=Idea.new
 	end
 
 	def index
 		@ideas = Idea.all
 	end
 
-
-
-
 	
 	def show
 		@idea = Idea.find(params[:id])
 	end
+
+	def create
+
+		@idea=Idea.create(idea_params)
+		if @idea.save
+			redirect_to idea_path(@idea)
+		else
+			render :new
+        end
+	end
+	
+	def idea_params
+		params.permit(:title, :description, :planned_to)
+	end
+	
+	def edit
+		@idea = Idea.find(params[:id])
+	end
+
+	def update
+		@idea = Idea.find(params[:id])
+		if @idea.update(idea_params)
+			redirect_to idea_path(@idea)
+		else
+			render :edit
+		end
+	end
+
 end
